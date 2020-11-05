36a014bf3399c041991500eeb444e7432e2ffd25
Andy Blackledge <andyblackledge@hotmail.com>
2020-11-05 21:26:27 +0000

    Tinkering away

--- src/functions.ts
+++ src/functions.ts
@@ -1,10 +1,13 @@
 import { EvaluationLog } from './EvaluationLog';
 
-export function numberConstant(value: number, log: EvaluationLog): number {
-    log.pushArgs('numberConstant', {value});
+let log: EvaluationLog;
 
-    log.pushResult('numberConstant', value);
-    return value;
+export function getLog(): EvaluationLog {
+    return log;
+}
+
+export function setLog(newLog: EvaluationLog): void {
+    log = newLog;
 }
 
 function mapArgs(args: any, ...argNames: string[]): any {
@@ -25,32 +28,113 @@ function mapArgs(args: any, ...argNames: string[]): any {
     return argObject;
 }
 
-export function squared(args: any, log: EvaluationLog): number {
+export function squared(args: any): number {
     log.pushArgs('squared', args);    
-
     const localArgs = mapArgs(args, 'value');
+
     const result = Math.pow(localArgs.value, 2);
     
     log.pushResult('squared', result);
     return result;
 }
 
-export function multiply(args: any, log: EvaluationLog): number {
-    log.pushArgs('multiply', args);
-    
+export function multiply(args: any): number {
+    log.pushArgs('multiply', args);    
     const localArgs = mapArgs(args, 'lhs', 'rhs'); // TODO 04Nov20: We could actually have a variable number here
+
     const result = localArgs.lhs * localArgs.rhs;
     
     log.pushResult('multiply', result);
     return result;
 }
 
-export function circleArea(args: any, log: EvaluationLog): number {
-    log.pushArgs('circleArea', args);
+export function ADD(args: any): number {
+    log.pushArgs('add', args);    
+    const localArgs = mapArgs(args, 'lhs', 'rhs'); // TODO 04Nov20: We could actually have a variable number here
+
+    const result = localArgs.lhs + localArgs.rhs;
     
+    log.pushResult('add', result);
+    return result;
+}
+
+export function circleArea(args: any): number {
+    log.pushArgs('circleArea', args);    
     const localArgs = mapArgs(args, 'radius');
-    const result = multiply({pi: Math.PI, radiusSquared: squared({radius: localArgs.radius}, log)}, log);
+
+    const result = multiply({
+        pi: Math.PI, 
+        radiusSquared: squared({radius: localArgs.radius})
+    });
     
     log.pushResult('circleArea', result);
+    return result;
+}
+
+export function GT(args: any): any {
+    log.pushArgs('gt', args);    
+    const localArgs = mapArgs(args, 'lhs', 'rhs');
+
+    const result = localArgs.lhs > localArgs.rhs;
+
+    log.pushResult('gt', result);
+    return result;    
+}
+
+export function OR(args: any): any {
+    log.pushArgs('or', args);    
+    const localArgs = mapArgs(args, 'lhs', 'rhs');
+
+    const result = localArgs.lhs || localArgs.rhs;
+
+    log.pushResult('or', result);
+    return result;    
+}
+
+export function VALUE(args: any): any {
+    log.pushArgs('value', args);    
+    const localArgs = mapArgs(args, 'value');
+
+    const result = localArgs.value;
+
+    log.pushResult('value', result);
+    return result;    
+}
+
+export function IFF(args: any): any {
+    log.pushArgs('if', args);    
+    const localArgs = mapArgs(args, 'boolValue', 'trueValue', 'falseValue');
+
+    const result = localArgs.boolValue ? localArgs.trueValue : localArgs.falseValue;
+
+    log.pushResult('if', result);
+    return result;        
+}
+
+export function residentialInterestRateToUse(args: any): number {
+
+    log.pushArgs('residentialInterestRateToUse', args);
+
+    const localArgs = mapArgs(args, 'initialRatePeriodInMonths', 'isAdditionalBorrowing', 'initialRate', 'reversionRate', 'stressRate');
+
+    const result =
+        IFF({
+            isInitialRateApplicable: OR({
+                initialRatePeriodGt60Months: GT({
+                    initialRatePeriodInMonths: localArgs.initialRatePeriodInMonths, 
+                    sixtyMonths: 60
+                }), 
+                isAdditionalBorrowing: localArgs.isAdditionalBorrowing
+            }),
+            trueValue: VALUE({
+                initialRate: localArgs.initialRate
+            }),
+            falseValue: ADD({
+                reversionRate: localArgs.reversionRate,
+                stressRate: localArgs.stressRate
+            })
+        });
+
+    log.pushResult('residentialInterestRateToUse', result);
     return result;
 }
--- test/tests.ts
+++ test/tests.ts
@@ -1,15 +1,32 @@
 import { expect } from 'chai';
 import { EvaluationLog } from '../src/EvaluationLog';
-import { circleArea } from '../src/functions';
+import { residentialInterestRateToUse, setLog } from '../src/functions';
 
 describe('Test class', () => {
 
     it('does something', () => {
 
-        const radius = 616;
+        setLog(new EvaluationLog());
         
-        const actualArea = circleArea({radius}, new EvaluationLog());
+        const calcArgs = {
+            initialRatePeriodInMonths: 36, 
+            isAdditionalBorrowing: false,
+            initialRate: 3,
+            reversionRate: 5,
+            stressRate: 1
+        };
 
-        expect(actualArea).to.equal(Math.PI * Math.pow(radius, 2));
+        let result = residentialInterestRateToUse(calcArgs);
+
+        expect(result).to.equal(6);
+
+        console.log();
+        console.log();
+
+        setLog(new EvaluationLog());
+
+        calcArgs.initialRatePeriodInMonths = 61;
+        result = residentialInterestRateToUse(calcArgs);
+        expect(result).to.equal(3);
     });
 });
